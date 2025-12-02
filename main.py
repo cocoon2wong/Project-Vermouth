08930d904cdb78902a6319d67f29b1315f32e2e0
Conghao Wong <conghaowong@icloud.com>
2025-12-02 09:56:25 +0800

    Add init files

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+@Author: Conghao Wong
+@Date: 2024-12-05 15:02:40
+@LastEditors: Conghao Wong
+@LastEditTime: 2025-12-02 09:54:08
+@Github: https://cocoon2wong.github.io
+@Copyright 2024 Conghao Wong, All Rights Reserved.
+"""
+
+import sys
+
+import qpid
+
+if __name__ == '__main__':
+    qpid.entrance(sys.argv)
